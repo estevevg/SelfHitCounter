81b35e55c68920b3188454faf3d70d7dd8384971
Esteve Verdura <esteve@unizite.com>
2022-05-10 19:25:28 +0200

    Final toutches and total

--- src/no-hit_counter.py
+++ src/no-hit_counter.py
@@ -50,14 +50,35 @@ class NoHitRun:
         self.slices[self.current_slice].hits +=1
 
     def next_phase(self):
-        self.slices[self.current_slice].time = int(datetime.now().timestamp() - self.init_time)
+        self.slices[self.current_slice].time = int(datetime.now().timestamp() - self.init_time - self.time_to_now())
         self.current_slice += 1
 
     def end(self):
         return len(self.slices) == self.current_slice
 
+    def time_to_now(self):
+        return sum([s.time for s in self.slices[:self.current_slice]]) if self.current_slice > 0 else 0 
+
+    def total_slice(self):
+        return sum([s.hits for s in self.slices]), sum([s.time for s in self.slices])
+
+    def total_pb(self):
+        return sum([pb.hits for pb in self.pb_slices]), sum([pb.time for pb in self.pb_slices])
+
+    def best_result(self):
+        return self.slices if self.total_slice() <= self.total_pb() else self.pb_slices
+
     def print_status(self):
-        return json.dumps(asdict(self), indent=2)
+        ts_h, ts_t = self.total_slice()
+        tpb_h, tpb_t = self.total_pb()
+        res = asdict(self)
+        res["total"] = {
+            "slice_hits": ts_h,
+            "slice_time": ts_t,
+            "pb_hits": tpb_h,
+            "pb_time": tpb_t
+        }
+        return json.dumps(res, indent=2)
 
 def create_slices(config, sl):
     slices = sl if len(sl) != 0 else config["splits"]
@@ -83,35 +104,13 @@ def save_state(state, path='src/data/state.json'):
     f.close()
 
 def save_pb_run(no_hit):
+    saved_slices =  no_hit.best_result() if no_hit.end() else no_hit.pb_slice
     run_config = {
         "run": no_hit.run,
         "current_slice": 0 if no_hit.end() else no_hit.current_slice,
-        "splits": []
+        "splits": [asdict(s) for s in saved_slices]
     }
-    for i, slice in enumerate(no_hit.slices):
-        pb = no_hit.pb_slices[i]
-        s = no_hit.slices[i]
 
-        if i <= no_hit.current_slice:
-            run_config["splits"].append(
-                asdict(
-                    Slice(
-                        s.name,
-                        s.hits if s.hits < pb.hits else pb.hits,
-                        s.time if s.time < pb.time else pb.time,
-                    )
-                )
-            )
-        else:
-            run_config["splits"].append(
-                asdict(
-                    Slice(
-                        s.name,
-                        pb.hits,
-                        pb.time,
-                    )
-                )
-            )
     save_state(json.dumps(run_config,  indent=2), 'src/data/run_config.json')
     save_state(json.dumps([] if no_hit.end() else [asdict(sl) for sl in no_hit.slices],  indent=2), 'src/data/run.json')
 
